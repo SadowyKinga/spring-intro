0d0041d25415e0e0ecfd368ac5e73ccca0437642
SadowyKinga <kinga.s.12@onet.pl>
2020-12-16 21:12:20 +0100

    Add conf-xml

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 public class MyApp {
 
@@ -8,8 +9,13 @@ public class MyApp {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
+        MessageService randomMessageService = applicationContext.getBean("randomMessageService", MessageService.class);
+
+        System.out.println("messageService - hashcode -> " + messageService.hashCode());
+        System.out.println("randomMessageService - hashcode -> " + randomMessageService.hashCode());
 
         System.out.println(messageService.getMessage());
+        System.out.println(randomMessageService.getMessage());
 
         applicationContext.close();
     }
